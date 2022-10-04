31cd8e882521d0a45fa8035a30370b65362a7399
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-10-04 21:36:30 +0300

    work on bugs

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import csv
 import json
 
 
-def create_json(json_gps, latitude, longitude):
-    x = dict(latitude = latitude, longitude = longitude)
-    json_gps.append(x)
-    return json_gps
+def create_json(latitude, longitude):
+    x = dict(latitude=latitude, longitude=longitude)
+    return x
 
 
 def read_data_for_new_task(filepath):
@@ -25,19 +24,15 @@ def add_new_row_to_csv(filepath):
 
 
 if __name__ == '__main__':
-    csv_file_path = 'C:/Users/Asus/Desktop/Python_coding/_Code/Read_and_send_file_DAS/coordinates.csv'
+    csv_file_path = 'coordinates.csv'
     coordinates_from_file = read_data_for_new_task(csv_file_path)
-    json_gps = []
     while True:
-        txt = input('Enter or send or end or readjson: ')
+        txt = input('Enter or send or end: ')
         if txt == '':
             gps_row = next(coordinates_from_file)
-            print(gps_row)
             coord1 = gps_row[0]
             coord2 = gps_row[1]
-            json_file = create_json(json_gps, coord1, coord2)
-        if txt == 'readjson':
-            print(json_file)
+            print(create_json(coord1, coord2))
         if txt == 'send':
             add_new_row_to_csv(csv_file_path)
         if txt == 'end':
