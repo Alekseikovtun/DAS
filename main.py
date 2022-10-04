372f16f53e116c65cb6c1bd09c6edc8b8e9ae2cf
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-10-04 22:55:02 +0300

    work on bugs 2

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def add_new_row_to_csv(filepath):
         longitude = input('Enter the longitude coordinates: ')
         str_with_coordinates = [latitude, longitude]
         writer.writerow(str_with_coordinates)
+    return str_with_coordinates
 
 
 if __name__ == '__main__':
@@ -28,10 +29,17 @@ if __name__ == '__main__':
     coordinates_from_file = read_data_for_new_task(csv_file_path)
     while True:
         txt = input('Enter or send or end: ')
-        if txt == '':
-            gps_row = next(coordinates_from_file)
-            coord1 = gps_row[0]
-            coord2 = gps_row[1]
+        try:
+            if txt == '':
+                gps_row = next(coordinates_from_file)
+                coord1 = gps_row[0]
+                coord2 = gps_row[1]
+                print(create_json(coord1, coord2))
+        except StopIteration:
+            print('Request for new coordinates...')
+            last_row = add_new_row_to_csv(csv_file_path)
+            coord1 = last_row[0]
+            coord2 = last_row[1]
             print(create_json(coord1, coord2))
         if txt == 'send':
             add_new_row_to_csv(csv_file_path)
