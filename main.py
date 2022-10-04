a943decc29dd2ef64e3906e48bcf5619addbdae7
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-10-04 18:24:21 +0300

    json_reading

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import csv
+import json
+
+
+def create_json(json_gps, latitude, longitude):
+    x = dict(latitude = latitude, longitude = longitude)
+    json_gps.append(x)
+    return json_gps
 
 
 def read_data_for_new_task(filepath):
@@ -20,10 +27,17 @@ def add_new_row_to_csv(filepath):
 if __name__ == '__main__':
     csv_file_path = 'C:/Users/Asus/Desktop/Python_coding/_Code/Read_and_send_file_DAS/coordinates.csv'
     coordinates_from_file = read_data_for_new_task(csv_file_path)
+    json_gps = []
     while True:
-        txt = input('Enter or send or end: ')
+        txt = input('Enter or send or end or readjson: ')
         if txt == '':
-            print(next(coordinates_from_file))
+            gps_row = next(coordinates_from_file)
+            print(gps_row)
+            coord1 = gps_row[0]
+            coord2 = gps_row[1]
+            json_file = create_json(json_gps, coord1, coord2)
+        if txt == 'readjson':
+            print(json_file)
         if txt == 'send':
             add_new_row_to_csv(csv_file_path)
         if txt == 'end':
