d7c119f2c0b6664a968e31e54a660fe6be0b2abd
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-10-16 18:11:17 +0300

    Done add_drone_status

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import service.db as db
 
 if __name__ == '__main__':
     while True:
-        txt = input('Enter or a or e: ')
+        txt = input('Enter or a or s or e: ')
         try:
             if txt == '':
                 db.read_data_for_new_task()
@@ -11,8 +11,14 @@ if __name__ == '__main__':
             # End-of-file error handler
             pass
         if txt == 'a':
-            latitude = input('Enter the latitude coordinates: ')
-            longitude = input('Enter the longitude coordinates: ')
+            latitude = input('Enter the latitude coordinate: ')
+            longitude = input('Enter the longitude coordinate: ')
             db.add_coordinate_to_db(latitude, longitude)
+        if txt == 's':
+            drone_id = input('Enter the drone id: ')
+            battery = input('Enter the battery level: ')
+            d_latitude = input('Enter the departure latitude coordinate: ')
+            d_longitude = input('Enter the departure longitude coordinate: ')
+            db.add_dron_status(drone_id, battery, d_latitude, d_longitude)
         if txt == 'e':
             break
--- service/db.py
+++ service/db.py
@@ -3,6 +3,7 @@ from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker
 
 from config import db_config
+from models.drone import DroneStatus
 from models.station import Task
 
 db = f'postgresql://{db_config.POSTGRES_USER}:{db_config.POSTGRES_PASSWORD}@localhost:{db_config.POSTGRES_OUT_PORT}/postgres'
@@ -15,8 +16,7 @@ session = Session()
 def read_data_for_new_task():
     next_task = session.query(Task, func.min(Task.id)).filter(Task.task_status_id==1).group_by(Task.id)
     next = next_task.all()[-1][1]
-    print(next)
-    pass
+    print(f'The next task has ID: {next}')
 
 
 def add_coordinate_to_db(add_latitude, add_longitude):
@@ -28,5 +28,16 @@ def add_coordinate_to_db(add_latitude, add_longitude):
     print('\n Data entered \n')
 
 
-def add_dron_info():
-    pass
+def add_dron_status(drone_id, battery, departure_lat, departure_long):
+    try:
+        new_info = DroneStatus(id=drone_id, battery_charge_lvl=battery, departure_latitude=departure_lat, departure_longitude=departure_long)
+        session.add_all([new_info])
+        session.commit()
+        print('\n Information about the drone has been received \n')
+        task_status_update = session.query(Task).filter(Task.latitude == departure_lat, Task.longitude == departure_long)
+        record = task_status_update.one()
+        record.task_status_id = 3
+        session.commit()
+        print('\n The task status has been updated \n')
+    except:
+        print('\n  Attention! A drone with an unknown task \n')
