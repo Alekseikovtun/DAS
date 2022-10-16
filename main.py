746468ba8c8119cf3316af40d0dc8829bf253b04
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-10-16 21:01:02 +0300

    Fixed bugs3

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import service.db as db
 if __name__ == '__main__':
     while True:
         txt = input('Enter or a or s or e: ')
-        try:
-            if txt == '':
-                db.read_data_for_new_task()
-                pass
-        except StopIteration:
-            # End-of-file error handler
-            pass
+        if txt == '':
+            gps = db.read_data_for_new_task()
+            print(gps)
         if txt == 'a':
             latitude = input('Enter the latitude coordinate: ')
             longitude = input('Enter the longitude coordinate: ')
--- service/db.py
+++ service/db.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 import sqlalchemy as sa
 from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker
@@ -6,22 +8,34 @@ from config import db_config
 from models.drone import DroneStatus
 from models.station import Task
 
-db = f'postgresql://{db_config.POSTGRES_USER}:{db_config.POSTGRES_PASSWORD}@localhost:{db_config.POSTGRES_OUT_PORT}/postgres'
+db = f'postgresql://{db_config.POSTGRES_USER}:{db_config.POSTGRES_PASSWORD}\
+    @localhost:{db_config.POSTGRES_OUT_PORT}/postgres'
 engine = sa.create_engine(db)
 Session = sessionmaker(bind=engine)
 connection = engine.connect()
 session = Session()
 
 
-def read_data_for_new_task():
-    next_task = session.query(Task, func.min(Task.id)).filter(Task.task_status_id==1).group_by(Task.id)
-    next = next_task.all()[-1][1]
-    print(f'The next task has ID: {next}')
+def read_data_for_new_task() -> Dict[str, Any]:
+    next_task = session.query(func.min(Task.id)).filter(
+        Task.task_status_id == 1
+    ).scalar()
+    next: Task = session.query(Task).filter(Task.id == next_task).all()[0]
+    result = {
+        'latitude': next.latitude,
+        'longitude': next.longitude
+    }
+    return result
 
 
 def add_coordinate_to_db(add_latitude, add_longitude):
     last_coord = session.query(func.max(Task.id)).scalar()
-    new_coord = Task(id=last_coord+1, latitude=add_latitude, longitude=add_longitude, task_status_id=1)
+    new_coord = Task(
+        id=last_coord+1,
+        latitude=add_latitude,
+        longitude=add_longitude,
+        task_status_id=1
+    )
     session.add_all([new_coord])
     session.commit()
     print('\n Data entered \n')
@@ -29,14 +43,22 @@ def add_coordinate_to_db(add_latitude, add_longitude):
 
 def add_dron_status(drone_id, battery, departure_lat, departure_long):
     try:
-        new_info = DroneStatus(id=drone_id, battery_charge_lvl=battery, departure_latitude=departure_lat, departure_longitude=departure_long)
+        new_info = DroneStatus(
+            id=drone_id,
+            battery_charge_lvl=battery,
+            departure_latitude=departure_lat,
+            departure_longitude=departure_long
+        )
         session.add_all([new_info])
         session.commit()
         print('\n Information about the drone has been received \n')
-        task_status_update = session.query(Task).filter(Task.latitude == departure_lat, Task.longitude == departure_long)
+        task_status_update = session.query(Task).filter(
+            Task.latitude == departure_lat,
+            Task.longitude == departure_long
+        )
         record = task_status_update.one()
         record.task_status_id = 3
         session.commit()
         print('\n The task status has been updated \n')
-    except:
+    except Exception:
         print('\n  Attention! A drone with an unknown task \n')
