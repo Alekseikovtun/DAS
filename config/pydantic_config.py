d8bf8fc1e62143100eacc7e72cf2f926a044134a
vdumchiviy <brain1999@mail.ru>
2023-02-11 01:11:41 -0500

    Pydantic config created, migrations work fine

--- config/pydantic_config.py
+++ config/pydantic_config.py
@@ -1,12 +1,23 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    MYSQL_ROOT_PASSWORD: set
-    MYSQL_USER: str
-    MYSQL_PASSWORD: str
-    MYSQL_DATABASE: str
-    MYSQL_OUT_PORT: int
-    MYSQL_HOST: str
+    DB_USER: str = Field(..., env="DB_USER")
+    DB_PASSWORD: str = Field(..., env="DB_PASSWORD")
+    DB_NAME: str = Field(..., env="DB_NAME")
+    DB_OUT_PORT: int = Field(..., env="DB_OUT_PORT")
+    DB_HOST: str = "localhost"
 
+    MYSQL_ROOT_PASSWORD: str = "something"
+    MYSQL_USER: str = DB_USER
+    MYSQL_PASSWORD: str = DB_PASSWORD
+    MYSQL_DATABASE: str = DB_NAME
+    MYSQL_OUT_PORT: int = DB_OUT_PORT
+    MYSQL_HOST: str = DB_HOST
+
+
+
+print("1")
 settings = Settings()
+print(settings.dict())
+print("2")
--- migrations.py
+++ migrations.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 from typing import Optional
-# import typer
+import typer
+from dotenv import load_dotenv
 
-# app = typer.Typer(add_completion=False)
+load_dotenv(".env")
 
-# @app.command(
-#     help="Run the latest migrations."
-# )
+app = typer.Typer(add_completion=False)
+
+@app.command(
+    help="Run the latest migrations."
+)
 def migrate():
-    #typer.echo("Migrating")
+    typer.echo("Migrating")
     import alembic.config #вот тут я б разобрался
     alembicArgs = [
         '--raiseerr',
@@ -17,12 +20,11 @@ def migrate():
     alembic.config.main(argv=alembicArgs)
 
 
-# @app.command(
-#     help="Create new migrations"
-# )
-#message: Optional[str] = typer.Argument("new migrations")
-def create_migrations():
-    #typer.echo(f"Creating migration '{message}'")
+@app.command(
+    help="Create new migrations"
+)
+def create_migrations(message: Optional[str] = typer.Argument("new migrations")):
+    typer.echo(f"Creating migration '{message}'")
     import alembic.config
     alembicArgs = [
         '--raiseerr',
