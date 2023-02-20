0066067d1b9162afa1bd9cd74ff7f31d1ab9909e
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2023-02-20 08:55:34 +0300

    deleting entries in models.__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .base import Base
-from .drone import DroneType, Drone, DroneStatus
-from .station import TaskStatus, Flight, Cargo, Task, Alert
-from .log import Log, DroneLog
+# from .base import Base
+# from .drone import DroneType, Drone, DroneStatus
+# from .station import TaskStatus, Flight, Cargo, Task, Alert
+# from .log import Log, DroneLog
